45f6e262a0818f1df5c54256811a5091b3c715b1
Muhammad Luqman <37179327+LuqmanFarooq@users.noreply.github.com>
2020-04-02 05:16:14 +0100

    Added more tests

--- myscript.py
+++ myscript.py
@@ -4,5 +4,14 @@
 
 import regex
 
-print(regex.match("a.b|b*","bbbbbbbbbbx"))
+tests = [
+    ["a.b|b*", "bbbbbb", True],
+    ["a.b|b*", "bbx", False],
+    ["a.b", "ab", True],
+    ["b**","b",True],
+    ["b*", "",True]
+]
+for test in tests:
+    assert regex.match(test[0],test[1]) == test[2],test[0] + \
+    ("should match " if test[2] else "should not match ")+ test[1]
 
